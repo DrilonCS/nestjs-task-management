9d8a8582ca677fd10b8eabe9cd53d6f301d442d4
Drilon Jashari <j.drilon99@gmail.com>
2024-09-16 18:24:00 +0200

    implemented getTaskById and deleteTaskById functions and endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,8 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { create } from 'domain';
 
 @Controller('tasks')
 export class TasksController {
@@ -13,8 +12,18 @@ export class TasksController {
         return this._tasksService.getAllTasks();
     }
 
+    @Get('/:id')
+    public getTaskById(@Param('id') id: string): Task {
+        return this._tasksService.getTaskById(id);
+    }
+
     @Post()
     public createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this._tasksService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    public deleteTaskById(@Param('id') id: string): void {
+        return this._tasksService.deleteTaskById(id);
     }
 }
